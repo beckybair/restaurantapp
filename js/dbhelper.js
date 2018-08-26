a51a2747d75d137329b8e4d787350a053348883f
beckybair <beckyhowen@gmail.com>
2018-08-26 17:27:41 -0500

    testing

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -166,7 +166,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return `/img/${restaurant.photograph}`;
+    return `/restaurant/img/${restaurant.photograph}`;
   }
 
   /**
